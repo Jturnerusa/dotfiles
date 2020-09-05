3ac168706db7b21855b6128bde4de0d562dec59d
Jturnerusa <jturnerusa@gmail.com>
2020-09-05 19:48:17 -0400

    init

---
+++ status.py
@@ -0,0 +1,92 @@
+#!/bin/python
+
+import sys, datetime, psutil
+from time import sleep, time
+
+class Time:
+
+	@staticmethod
+	def date():
+		return datetime.datetime.now().strftime(f"%a %b %-e %I:%M:%S %p")
+
+
+class CPU:
+
+	@staticmethod
+	def coretemp():
+		return str(round(psutil.sensors_temperatures(fahrenheit=False)["k10temp"][0][1], 1))
+
+	@staticmethod
+	def freq():
+		return str(round(int(psutil.cpu_freq(percpu=False)[0])/1000, 1))
+
+	@staticmethod
+	def usage():
+		u = psutil.cpu_percent(interval=0.1, percpu=False)
+		return f"{u}%"
+		
+class DISK:
+	
+	@staticmethod
+	def disk_total(mnt):
+		return f"{round(psutil.disk_usage(mnt).total/1000**3, 1)}"
+	
+	def disk_used(mnt):
+		return f"{round(psutil.disk_usage(mnt).used/1000**3, 1)}"
+	
+
+class RAM:
+
+	@staticmethod
+	def ram_total():
+		return f"{round(psutil.virtual_memory().total/1000**3, 1)}"
+
+	@staticmethod
+	def ram_used():
+		return f"{round(psutil.virtual_memory().used/1000**3, 1)}"
+
+	@staticmethod
+	def swap_total():
+		return f"{round(psutil.swap_memory().total/1000**3, 2)}"
+
+	@staticmethod
+	def swap_used():
+		return f"{round(psutil.swap_memory().used/1000**3, 2)}"
+		
+class Network:
+	
+	time_down = 0
+	time_up = 0
+	bytes_recv = 0
+	bytes_sent = 0
+	
+	@staticmethod
+	def get_bytes_down():
+		bytes_delta = psutil.net_io_counters().bytes_recv - Network.bytes_recv
+		time_delta = time() - Network.time_down
+		download_speed = (bytes_delta / time_delta)//1000
+		Network.time_down = time()
+		Network.bytes_recv = psutil.net_io_counters().bytes_recv
+		return download_speed
+
+	@staticmethod
+	def get_bytes_up():
+		bytes_delta = psutil.net_io_counters().bytes_sent - Network.bytes_sent
+		time_delta = time() - Network.time_up
+		upload_speed = (bytes_delta / time_delta)//1000
+		Network.time_up = time()
+		Network.bytes_sent = psutil.net_io_counters().bytes_sent
+		return upload_speed
+
+
+class Battery:
+
+	@staticmethod
+	def percent():
+		return int(psutil.sensors_battery().percent)
+
+
+while True:
+	sys.stdout.write(f"Network: [UP:{Network.get_bytes_up()}KB/s DOWN:{Network.get_bytes_down()}KB/s] | CPU: [{CPU.freq()} GHz] [{CPU.coretemp()} C] | CRYPTROOT: [{DISK.disk_total('/')} GB/{DISK.disk_used('/')} GB] | CRYPTSSD: [{DISK.disk_total('/mnt/ssd/')} GB/{DISK.disk_used('/mnt/ssd/')} GB] | RAM: [{RAM.ram_total()} GB/{RAM.ram_used()} GB] | SWAP: [{RAM.swap_total()} GB/{RAM.swap_used()} GB] | TIME: [{Time.date()}] ")
+	sys.stdout.flush()
+	sleep(.75)
